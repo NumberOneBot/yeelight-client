5880fdcf9eee16022012ec95e7c25c961ca92bc5
Alex Strelets <alex.strelets@gmail.com>
2026-03-28 20:10:09 +0200

    fix(docs): reorder pages

--- packages/docs/content/_meta.ts
+++ packages/docs/content/_meta.ts
@@ -1,8 +1,8 @@
 export default {
   index: 'Overview',
   api: 'API Reference',
-  cli: 'CLI',
-  devices: 'Devices',
   protocol: 'Protocol Reference',
-  commands: 'Commands Reference'
+  commands: 'Commands Reference',
+  cli: 'CLI',
+  devices: 'Devices'
 }
